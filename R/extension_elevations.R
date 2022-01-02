49810b3ff88d23061a4da2f1208741e3f05a7210
Liam Bolton <liambolton@liams-mbp.home>
2022-01-02 12:40:47 +0000

    Commit

--- R/extension_elevations.R
+++ R/extension_elevations.R
@@ -1,6 +1,6 @@
-#' Building Elevations for Rooftop Extensions
+#' Building Elevations for Potential Rooftop Extensions
 #'
-#' Function for creating building elevation small multiples for rooftop extensions using permutations.
+#' Function for creating building elevation small multiples for potential rooftop extensions using permutations.
 #'
 #' @param df Data frame containing building elevations including the number of storeys
 #' @param max_permitted_storey Maximum permitted storey
