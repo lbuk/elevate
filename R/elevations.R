343d402f72142346f2d8de50039bc45fdaf459ae
Liam Thomas Bolton <liamthomasbolton@gmail.com>
2023-09-03 20:11:25 +0100

    include new options to generate datasets of permutations

--- R/elevations.R
+++ R/elevations.R
@@ -5,12 +5,17 @@
 #' @param max_storey Maximum permitted storey per building
 #' @param min_storey Minimum permitted storey per building
 #' @param nbuildings Number of buildings
-#' @return Small multiple charts depicting building elevations and a print-out of the number of potential combinations in the console
+#' @param output Output of function. Input 'plot' for charts. Input 'data' for a data frame containing the number of storeys per building
+#' @return Multiple charts depicting building elevations and a print-out of the number of potential combinations in the console or a data frame containing the number of storeys per building
 #' @examples
-#' elevations(nbuildings = 6, min_storey = 4, max_storey = 5)
+#' # Charts based on the number of buildings, min storey and max storey
+#' elevations(nbuildings = 6, min_storey = 4, max_storey = 5, output = 'plot')
+#' 
+#' # Dataset containing permutations of storeys per building
+#' elevations(nbuildings = 8, min_storey = 4, max_storey = 6, output = 'data')
 #' @export
 
-elevations = function(nbuildings, min_storey, max_storey) {
+elevations = function(nbuildings, min_storey, max_storey, output = 'plot') {
   
   if(nbuildings <= 1) {
     stop('nbuildings should be > 1', call. = F)
@@ -38,33 +43,49 @@ elevations = function(nbuildings, min_storey, max_storey) {
   df_el$max_s = apply(df_el[1:nbuildings], 1, max, na.rm = T)
   df_el = df_el[order(df_el$sum_s, df_el$max_s),]
   
-  n = nrow(df_el)
-  
-  cat("Number of potential combinations:", n, " ")
-  
-  y = ceiling(n / 10)
-  
-  m = max_storey
-  
-  par(mfrow = c(y, 10), mar = c(0.2, 0.2, 2, 0.2))
-  
-  invisible(sapply(1:length(df_el[,1]), function(i) {
-    build = df_el[i, 1:nbuildings]
+  if(output == 'plot') {
+    n = nrow(df_el)
     
-    barplot(as.numeric(build), ylim = c(0, m), col = "#D10081", border = F, space = 0.075, axes = F)
+    cat("Number of potential combinations:", n, " ")
     
-    abline(h = seq(1, m, 1), col = "#ffffff", lwd = 1.5)
-  }))
-  
-  plot_legend = function(...) {
-    opar = par(fig = c(0, 1, 0, 1), mar = c(0, 0, 0, 0), oma = c(0, 0, 0, 0), new = T)
+    y = ceiling(n / 10)
     
-    on.exit(par(opar))
+    m = max_storey
     
-    plot(0, 0, type = 'n', xaxt = 'n', yaxt = 'n', bty = 'n')
+    par(mfrow = c(y, 10), mar = c(0.2, 0.2, 2, 0.2))
     
-    legend(...)
+    invisible(sapply(1:length(df_el[,1]), function(i) {
+      build = df_el[i, 1:nbuildings]
+      
+      barplot(as.numeric(build), ylim = c(0, m), col = "#D10081", border = F, space = 0.075, axes = F)
+      
+      abline(h = seq(1, m, 1), col = "#ffffff", lwd = 1.5)
+    }))
+    
+    plot_legend = function(...) {
+      opar = par(fig = c(0, 1, 0, 1), mar = c(0, 0, 0, 0), oma = c(0, 0, 0, 0), new = T)
+      
+      on.exit(par(opar))
+      
+      plot(0, 0, type = 'n', xaxt = 'n', yaxt = 'n', bty = 'n')
+      
+      legend(...)
+    }
+    
+    plot_legend("topleft", legend = "Storey", cex = 1.05, pch = 15, col = "#D10081", text.font = 2, bty = 'n', horiz = T)
+    
+  } else if(output == 'data') {
+    el_data = df_el
+    
+    el_data = el_data[,1:nbuildings]
+    
+    colnames(el_data) = gsub("Var", "building_", as.character(colnames(el_data)))
+    
+    rownames(el_data) = NULL
+    
+    el_data
+    
+  } else {
+    stop('error in output parameter', call. = F)
   }
-  
-  plot_legend("topleft", legend = "Storey", cex = 1.05, pch = 15, col = "#D10081", text.font = 2, bty = 'n', horiz = T)
 }
--- R/extension_elevations.R
+++ R/extension_elevations.R
@@ -6,18 +6,24 @@
 #' @param df Data frame of existing elevations. Each column should represent a building and the data frame should contain one row with the number of storeys per building
 #' @param max_permitted_storey Maximum permitted storey per building
 #' @param max_permitted_extstorey Maximum permitted storey per extension
-#' @return Multiple bar charts depicting potential rooftop extension elevations ordered by density as well as a print-out of the number of potential combinations in the console
+#' @param output Output of function. Input 'plot' for charts. Input 'data' for a data frame containing the number of storeys per building. Defaults to 'plot'
+#' @return Multiple charts depicting potential rooftop extension elevations ordered by density as well as a print-out of the number of potential combinations in the console or a data frame containing the number of storeys per building
 #' @examples
+#' storeys_i = c(3, 3, 4) # Establish the number of storeys per existing building
+#' 
 #' # Visualise potentials based on the maximum number of storeys per extension
-#' storeys_i = c(3, 3, 4)
-#' extension_elevations(storeys = storeys_i, max_permitted_extstorey = 2)
+#' extension_elevations(storeys = storeys_i, max_permitted_extstorey = 2, output = 'plot')
+#' 
+#' # Generate a dataset containing permutations of storeys per building
+#' extension_elevations(storeys = storeys_i, max_permitted_extstorey = 1, output = 'data')
+#' 
+#' storeys_ii = c(3, 2, 2, 3, 4) # Establish the number of storeys per existing building
 #' 
 #' # Chart potential extensions using the maximum number of storeys per building
-#' storeys_ii = c(3, 2, 2, 3, 4)
-#' extension_elevations(storeys = storeys_ii, max_permitted_storey = 4)
+#' extension_elevations(storeys = storeys_ii, max_permitted_storey = 4, output = 'plot')
 #' @export
 
-extension_elevations = function(storeys = NULL, df = NULL, max_permitted_storey = NULL, max_permitted_extstorey = NULL) {
+extension_elevations = function(storeys = NULL, df = NULL, max_permitted_storey = NULL, max_permitted_extstorey = NULL, output = 'plot') {
   
   if(is.null(storeys) != T && is.null(df) != T) {
     stop('input storeys or df', call. = F)
@@ -87,61 +93,77 @@ extension_elevations = function(storeys = NULL, df = NULL, max_permitted_storey 
   
   df_ext = df_ext[-1,]
   
-  if(nrow(df_ext) == 0) {
-    n = 0
+  if(output == 'plot') {
+    if(nrow(df_ext) == 0) {
+      n = 0
+      
+    } else {
+      n = nrow(df_ext)
+    }
+    
+    cat("Number of potential combinations:", n, " ")
+    
+    if(n > 50) {
+      x = 8
+      
+    } else {
+      x = 6
+    }
+    
+    y = ceiling(n / x)
+    
+    if(is.null(max_permitted_extstorey) != T && is.null(max_permitted_storey) != T && max_permitted_storey >= max(df)) {
+      m = max_permitted_storey
+      
+    } else if(is.null(max_permitted_extstorey) != T && is.null(max_permitted_storey) != T && max_permitted_storey < max(df)) {
+      m = max(df)
+      
+    } else if(is.null(max_permitted_extstorey) != T && is.null(max_permitted_storey)) {
+      m = max(df) + max_permitted_extstorey
+      
+    } else if(is.null(max_permitted_storey) != T && is.null(max_permitted_extstorey) && max_permitted_storey >= max(df)) {
+      m = max_permitted_storey
+      
+    } else {
+      m = max(df)
+    }
+    
+    par(mfrow = c(y, x), mar = c(0.175, 0.175, 2.4, 0.175))
+    
+    invisible(sapply(1:length(df_ext[,1]), function(i) {
+      ext = df_ext[i, 1:nbuildings]
+      
+      barplot(as.numeric(ext[,1:nbuildings]), ylim = c(0, m), col = "#00DCF5", border = F, space = 0.075, axes = F)
+      
+      barplot(as.numeric(df[,1:ncol(df)]), ylim = c(0, max(df)), col = "#D9F045", border = F, space = 0.075, axes = F, add = T)
+      
+      abline(h = seq(1, m, 1), col = "#ffffff", lwd = 2)
+    }))
+    
+    plot_legend = function(...) {
+      opar = par(fig = c(0, 1, 0, 1), mar = c(0, 0, 0, 0), oma = c(0, 0, 0, 0), new = T)
+      
+      on.exit(par(opar))
+      
+      plot(0, 0, type = 'n', xaxt = 'n', yaxt = 'n', bty = 'n')
+      
+      legend(...)
+    }
+    
+    plot_legend("topleft", legend = rev(c("Existing Storey", "Potential Storey")), cex = 0.9, pch = 15, col = c("#00DCF5", "#D9F045"), text.font = 2, bty = 'n', horiz = F)
+    
+  } else if(output == 'data') {
+    ext_data = df_ext
+    
+    ext_data = ext_data[,1:nbuildings]
+    
+    colnames(ext_data) = gsub("Var", "building_", as.character(colnames(ext_data)))
+    
+    rownames(ext_data) = NULL
+    
+    ext_data
     
   } else {
-    n = nrow(df_ext)
+    stop('error in output parameter', call. = F)
   }
-  
-  cat("Number of potential combinations:", n, " ")
-  
-  if(n > 50) {
-    x = 8
-    
-  } else {
-    x = 6
-  }
-  
-  y = ceiling(n / x)
-  
-  if(is.null(max_permitted_extstorey) != T && is.null(max_permitted_storey) != T && max_permitted_storey >= max(df)) {
-    m = max_permitted_storey
-    
-  } else if(is.null(max_permitted_extstorey) != T && is.null(max_permitted_storey) != T && max_permitted_storey < max(df)) {
-    m = max(df)
-    
-  } else if(is.null(max_permitted_extstorey) != T && is.null(max_permitted_storey)) {
-    m = max(df) + max_permitted_extstorey
-    
-  } else if(is.null(max_permitted_storey) != T && is.null(max_permitted_extstorey) && max_permitted_storey >= max(df)) {
-    m = max_permitted_storey
-    
-  } else {
-    m = max(df)
-  }
-  
-  par(mfrow = c(y, x), mar = c(0.175, 0.175, 2.4, 0.175))
-  
-  invisible(sapply(1:length(df_ext[,1]), function(i) {
-    ext = df_ext[i, 1:nbuildings]
-    
-    barplot(as.numeric(ext[,1:nbuildings]), ylim = c(0, m), col = "#00DCF5", border = F, space = 0.075, axes = F)
-    
-    barplot(as.numeric(df[,1:ncol(df)]), ylim = c(0, max(df)), col = "#D9F045", border = F, space = 0.075, axes = F, add = T)
-    
-    abline(h = seq(1, m, 1), col = "#ffffff", lwd = 2)
-  }))
-  
-  plot_legend = function(...) {
-    opar = par(fig = c(0, 1, 0, 1), mar = c(0, 0, 0, 0), oma = c(0, 0, 0, 0), new = T)
-    
-    on.exit(par(opar))
-    
-    plot(0, 0, type = 'n', xaxt = 'n', yaxt = 'n', bty = 'n')
-    
-    legend(...)
-  }
-  
-  plot_legend("topleft", legend = rev(c("Existing Storey", "Potential Storey")), cex = 0.9, pch = 15, col = c("#00DCF5", "#D9F045"), text.font = 2, bty = 'n', horiz = F)
 }
